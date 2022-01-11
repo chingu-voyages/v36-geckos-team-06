ebfec7e83b56a1f33b260ad07ad76c83a92f6ee0
Kolade Afode <kolade.afode@yahoo.com>
2022-01-11 14:50:39 +0100

    fix: untracking .env

---
+++ src/components/common/Layout.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const LayoutStyled = styled.main`
+  max-width: 1440px;
+  margin: 0 auto;
+  padding: 0px 120px;
+  display: flex;
+  flex-direction: column;
+  gap: 80px;
+`;
