3512de1b76d7a4590902ef12ba3c08e004b7c087
Kolade Afode <kolade.afode@yahoo.com>
2022-01-12 23:02:48 +0100

    feat: add landlord sign in page

--- src/pages/landlord/signin.jsx
+++ src/pages/landlord/signin.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import Link from 'next/link';
+import Layout from '../../components/landlordSignin/Layout';
+import { Form, InputContainer, Input, Button, Info } from '../../components/landlordSignin/Form';
 
 const SignIn = () => (
-  <div>
-    <h1>Sign In</h1>
-  </div>
+  <Layout>
+    <Form>
+      <Info>
+        <h1>SIGN IN</h1>
+        <p>LANDLORD</p>
+      </Info>
+
+      <InputContainer>
+        <label htmlFor="email">EMAIL ADDRESS</label>
+        <Input name="email" type="email" id="email" />
+      </InputContainer>
+      <InputContainer>
+        <label htmlFor="password">PASSWORD</label>
+        <Input name="password" type="password" id="password" />
+      </InputContainer>
+      <Button> Sign In</Button>
+      <Link href="/">
+        <a href>GO BACK</a>
+      </Link>
+    </Form>
+  </Layout>
 );
 
 export default SignIn;
