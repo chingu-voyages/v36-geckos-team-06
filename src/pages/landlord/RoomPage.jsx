df81191a99a6083c00e7016a04fb0e50143a700d
Wekiban <wes.banyard@googlemail.com>
2022-01-19 16:48:09 +0100

    feat: completed layout for RoomPage

--- src/pages/landlord/RoomPage.jsx
+++ src/pages/landlord/RoomPage.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import { GiFamilyHouse, GiMoneyStack, GiSpanner, GiWaterDrop, GiElectric } from 'react-icons/gi';
+import { FaFileContract } from 'react-icons/fa';
+import { AiFillCar, AiOutlineCarryOut } from 'react-icons/ai';
+import { BsHouse, BsWrench, BsCircle } from 'react-icons/bs';
+import Image from 'next/image';
 import RepairSection from '../../components/common/RepairSection';
 import Layout from '../../components/common/Layout';
+import profilePic from '../../../public/profilePic.jpg';
+import { REPAIR_DATA, PROPERTY_DATA } from '../../mockData';
 
-import { REPAIR_DATA } from '../../mockData';
-
+const roomData = PROPERTY_DATA[0].rooms[0];
+console.log(roomData);
+// name charges occupant
 const roomOneRepairs = REPAIR_DATA.filter((repair) => repair.room === 'room 1');
-
-console.log(roomOneRepairs);
 
 const RoomHeader = styled.header`
   display: flex;
@@ -25,11 +31,11 @@ const RoomHeader = styled.header`
 `;
 
 const Button = styled.button`
-  width: 264px;
-  height: 104px;
+  width: 200px;
+  height: 60px;
   background: white;
   border-radius: 16px;
-  font-size: 32px;
+  font-size: 1.6rem;
   font-weight: bold;
   text-transform: uppercase;
   cursor: pointer;
@@ -49,23 +55,92 @@ const TextContainer = styled.div`
   line-height: 1.1;
   color: white;
   h1 {
-    font-size: 96px;
+    font-size: 3rem;
   }
   p {
-    font-size: 36px;
+    font-size: 1.6rem;
   }
+`;
+
+const ProfileSection = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const ImageInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  h4 {
+    font-size: 2rem;
+  }
+  p {
+    font-size: 1.5rem;
+  }
+`;
+
+const ImageWrapper = styled.div`
+  height: 300px;
+  overflow: hidden;
+  width: 300px;
+  border-radius: 10px;
+`;
+
+const ProfileImage = styled(Image)``;
+
+const CurrentBalances = styled.div`
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  padding: 20px;
+  color: #666;
+  font-size: 1.5rem;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  height: 300px;
+  background: #491f1e;
+  border-radius: 10px;
+  height: 532px;
+`;
+
+const CurrentBalanceTile = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  height: 100%;
+  border-radius: 10px;
+  background: white;
+  padding: 15px;
+  gap: 15px;
+  p {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    font-size: 1rem;
+  }
+`;
+
+const TileHeader = styled.h4`
+  font-weight: 300;
+  width: 100%;
+  gap: 10px;
+  display: flex;
+  justify-content: 'center';
+  align-items: center;
 `;
 
 const RepairsHeading = styled.h2`
   text-transform: uppercase;
-  font-size: 72px;
+  font-size: 2rem;
 `;
 
 const RepairsContainer = styled.div`
   display: flex;
   width: 100%;
   flex-direction: column;
-  gap: 32px;
+  gap: 1rem;
   max-height: 750px;
   overflow: scroll;
 `;
@@ -80,6 +155,65 @@ const RoomPage = () => (
       </TextContainer>
       <Button>Edit Room</Button>
     </RoomHeader>
+    <ProfileSection>
+      <ImageInfoContainer>
+        <ImageWrapper>
+          <ProfileImage src={profilePic} />
+        </ImageWrapper>
+        <h4>Rose Smith</h4>
+        <p>Rose@gmail.com</p>
+        <p>07715572035</p>
+      </ImageInfoContainer>
+      <CurrentBalances>
+        <CurrentBalanceTile>
+          <TileHeader>
+            <FaFileContract /> Contract
+          </TileHeader>
+          <p style={{ borderBottom: '1px solid #eaeaea', paddingBottom: '10px' }}>
+            <BsCircle style={{ color: 'green' }} />
+            Start: 20/03/2021
+          </p>
+          <p style={{ borderBottom: '1px solid #eaeaea', paddingBottom: '10px' }}>
+            <BsCircle style={{ color: '#491f1e' }} />
+            End: 20/03/2022
+          </p>
+          <p>
+            <BsCircle style={{ color: 'orange' }} />
+            Remaining: 40 days
+          </p>
+        </CurrentBalanceTile>
+        <CurrentBalanceTile>
+          <TileHeader>
+            <BsWrench />
+            Repairs
+          </TileHeader>
+        </CurrentBalanceTile>
+        <CurrentBalanceTile>
+          <TileHeader>
+            <BsHouse /> Utilities
+          </TileHeader>
+        </CurrentBalanceTile>
+        <CurrentBalanceTile>
+          <TileHeader>
+            <BsHouse />
+            Rent
+          </TileHeader>
+        </CurrentBalanceTile>
+
+        <CurrentBalanceTile>
+          <TileHeader>
+            <AiFillCar />
+            Parking
+          </TileHeader>
+        </CurrentBalanceTile>
+        <CurrentBalanceTile>
+          <TileHeader>
+            <AiOutlineCarryOut />
+            Overview
+          </TileHeader>
+        </CurrentBalanceTile>
+      </CurrentBalances>
+    </ProfileSection>
     <RepairsHeading>Repairs</RepairsHeading>
     <RepairsContainer>
       {roomOneRepairs.map((repair) => (
