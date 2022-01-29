4798175fc005171e477db5a015d5edfe3a3fb489
Kolade Afode <kolade.afode@yahoo.com>
2022-01-29 20:18:24 +0100

    feat: connect tentnat page to database

--- src/components/tenant/Dashboard.jsx
+++ src/components/tenant/Dashboard.jsx
@@ -11,20 +11,28 @@ export const ProfileImageContainer = styled.div`
 export const CardContainer = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   display: flex;
   width: 100%;
   background: linear-gradient(#491f1e, #1f0100);
   border-radius: 16px;
-  padding: 10px;
-  padding-bottom: 30px;
-  gap: 20px;
+  padding: 44px;
+  gap: 44px;
 `;
 
 export const Card = styled.div`
-  height: 320px;
-  width: 300px;
+  width: 100%;
   border-radius: 16px;
   background: #fff;
   padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+export const CardContent = styled.div`
+  width: 100%;
+  border: solid 1px #1f0100;
+  border-radius: 16px;
+  padding: 24px;
 `;
--- src/pages/tenant/dashboard.jsx
+++ src/pages/tenant/dashboard.jsx
@@ -1,21 +1,25 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-undef */
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useQuery } from '@apollo/client';
 import { getUserFromLocalStorage } from '../../../utils';
 import profilePic from '../../../public/profilePic.jpg';
 import { REPAIR_DATA } from '../../mockData';
-
 import Layout from '../../components/common/Layout';
 import AddRepair from '../../components/tenant/forms/AddRepair';
 import { HeaderStyled, HeaderButton } from '../../components/common/Header';
 import { RepairsContainer, RepairsHeading } from '../../components/common/Repairs';
 import Repair from '../../components/common/Repair';
-import { ProfileImageContainer, CardContainer, Card } from '../../components/tenant/Dashboard';
+import {
+  ProfileImageContainer,
+  CardContainer,
+  Card,
+  CardContent,
+} from '../../components/tenant/Dashboard';
 import Button from '../../components/common/Button';
+import { GET_TENANT } from '../../../services/query';
 
 // name charges occupant
 const roomOneRepairs = REPAIR_DATA.filter((repair) => repair.room === 'room 1');
@@ -24,7 +28,13 @@ const Dashboard = () => {
   const [createRepair, setCreateRepair] = useState(false);
   const [currentRepair, setCurrentRepair] = useState({});
   const tenant = getUserFromLocalStorage(`authTenant`);
-  const router = useRouter();
+  const Router = useRouter();
+
+  const { loading, error, data } = useQuery(GET_TENANT, {
+    variables: { email: tenant?.email },
+  });
+
+  console.log(data?.tenant?.charges);
 
   useEffect(() => {
     if (!tenant) {
@@ -35,38 +45,58 @@ const Dashboard = () => {
     return null;
   }, [tenant]);
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
+
   return (
-    <Layout>
+    <>
       {createRepair && <AddRepair setCreateRepair={setCreateRepair} />}
-      <HeaderStyled
-        direction="row"
-        justify="space-between"
-        align="flex-end"
-        bg="https://ik.imagekit.io/txobowsaxlc/dashbaord-pic-main_Jasm7l6BB.png?ik-sdk-version=javascript-1.4.3&updatedAt=1642269286358"
-      >
-        <ProfileImageContainer>
-          <Image layout="responsive" src={profilePic} />
-        </ProfileImageContainer>
-        <Button style={{ width: '272px' }} color="white" onClick={() => setCreateRepair(true)}>
-          Add Repair
-        </Button>
-      </HeaderStyled>
-      <CardContainer>
-        <Card>1</Card>
-        <Card>2</Card>
-      </CardContainer>
-      <RepairsHeading>Repairs</RepairsHeading>
-      <RepairsContainer>
-        {roomOneRepairs.map((repair) => (
-          <Repair
-            key={repair.id}
-            repair={repair}
-            setCurrentRepair={setCurrentRepair}
-            setUpdateRepair={setCreateRepair}
-          />
-        ))}
-      </RepairsContainer>
-    </Layout>
+
+      <Layout>
+        <HeaderStyled
+          direction="row"
+          justify="space-between"
+          align="flex-end"
+          bg={
+            data?.tenant?.property?.fullImage ||
+            'https://ik.imagekit.io/txobowsaxlc/dashbaord-pic-main_Jasm7l6BB.png?ik-sdk-version=javascript-1.4.3&updatedAt=1642269286358'
+          }
+        >
+          <ProfileImageContainer>
+            <Image layout="fill" src={data?.tenant?.occupant?.avatar || profilePic} />
+          </ProfileImageContainer>
+          <Button style={{ width: '272px' }} color="white" onClick={() => setCreateRepair(true)}>
+            Add Repair
+          </Button>
+        </HeaderStyled>
+        <CardContainer>
+          <Card>
+            <CardContent>TEST</CardContent>
+            <CardContent>TEST</CardContent>
+            <CardContent>TEST</CardContent>
+            <CardContent>TEST</CardContent>
+            <CardContent>TEST</CardContent>
+          </Card>
+          <Card>
+            <CardContent>${data?.tenant?.charges.rent}</CardContent>
+            <CardContent>${data?.tenant?.charges.water}</CardContent>
+            <CardContent>${data?.tenant?.charges.electricity}</CardContent>
+            <CardContent>${data?.tenant?.charges.parking}</CardContent>
+          </Card>
+        </CardContainer>
+        <RepairsHeading>Repairs</RepairsHeading>
+        <RepairsContainer>
+          {roomOneRepairs.map((repair) => (
+            <Repair
+              key={repair.id}
+              repair={repair}
+              setCurrentRepair={setCurrentRepair}
+              setUpdateRepair={setCreateRepair}
+            />
+          ))}
+        </RepairsContainer>
+      </Layout>
+    </>
   );
 };
 
